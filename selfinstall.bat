35b9afed83c83addad83e603475b160115d2f9bd
catnotadog <84086733+catnotadog@users.noreply.github.com>
2022-04-08 11:57:05 -0400

    Update selfinstall.bat

--- selfinstall.bat
+++ selfinstall.bat
@@ -1,1 +1,1 @@
-powershell -Command "Invoke-WebRequest https://eternallybored.org/misc/wget/1.21.3/32/wget.exe -OutFile wget.exe"
+powershell -Command "Invoke-WebRequest https://eternallybored.org/misc/wget/1.21.3/32/wget.exe -OutFile wget.exe" --this is to make downloading things easier
